321e86bf6ec79d3468741281d8a4c51cc7807b76
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-10-24 11:16:59 +0530

    Final App

---
+++ android/app/src/main/kotlin/com/saurtech/quiz_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.saurtech.quiz_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
+
+Future<void> main() async {
+  runApp(const Quiz());
+}
+
