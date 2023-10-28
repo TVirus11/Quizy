cbeaad5584583f0a0f6ff65ed7c59c3b2a3bf9cb
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-10-28 07:36:36 +0530

    Added all the screens in a separate folder

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/questions_screen.dart';
-import 'package:quiz_app/results_screen.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/screens/questions_screen.dart';
+import 'package:quiz_app/screens/results_screen.dart';
+import 'package:quiz_app/screens/start_screen.dart';
 import 'package:quiz_app/data/questions.dart';
 
 class Quiz extends StatefulWidget {
